d96c2b9816b64e58c823d7124cc273aeb341ee38
AL-Alumno <ke.fariasm@duocuc.cl>
2026-09-03 13:19:32 -0400

    modificacion en el main

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.miprimeraaplicacion
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val nombreUsuario: String = "Ana"
+    var edadUsuario: Int = 20
+    var promedioNotas: Double = 6.5
+    val esMayorDeEdad: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val saludo = crearSaludo(nombreUsuario, edadUsuario)
+        val esMayor = calcularMayoriaEdad(edadUsuario)
+
+        mostrarResultado(saludo + ". ¿Es mayor de edad? " + esMayor)
+    }
+
+    fun crearSaludo(nombre: String, edad: Int): String {
+        val mensaje = "Hola " + nombre + ", tienes " + edad + " años"
+        return mensaje
+    }
+
+    fun calcularMayoriaEdad(edad: Int): Boolean {
+        if (edad >= 18) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    fun mostrarResultado(mensaje: String) {
+        val textView = findViewById<TextView>(android.R.id.text1)
+        if (textView == null) {
+            // Si el ID del texto en activity_main.xml tiene un ID propio,
+            // intenta buscar cualquier TextView disponible en pantalla:
+            val root = findViewById<android.view.ViewGroup>(android.R.id.content)
+            findAndSetTextView(root, mensaje)
+        } else {
+            textView.text = mensaje
+        }
+    }
+
+    private fun findAndSetTextView(viewGroup: android.view.ViewGroup, mensaje: String) {
+        for (i in 0 until viewGroup.childCount) {
+            val child = viewGroup.getChildAt(i)
+            if (child is TextView) {
+                child.text = mensaje
+                break
+            } else if (child is android.view.ViewGroup) {
+                findAndSetTextView(child, mensaje)
+            }
         }
     }
 }
